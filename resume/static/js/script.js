2b0144c8b7239bf875cd94968c74e5ca7f9e38e9
MarcusKun <1274662304@qq.com>
2017-08-04 16:21:45 +0800

    修改了JS文件

--- resume/static/js/script.js
+++ resume/static/js/script.js
@@ -13,6 +13,7 @@ if (!String.prototype.format) {
 
 
 jQuery(document).ready(function($) {
+	$("*").removeAttr('contenteditable');
     $(".left-label").attr('contenteditable', 'true');
     $(".label-value").attr('contenteditable', 'true');
     $(".info-title").attr('contenteditable', 'true');
